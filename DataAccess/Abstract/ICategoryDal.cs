0d47bb87bc56a6256ddb4933ee0e2042c9286a98
Ozan Kazanç <ozan.kazanc@hotmail.com>
2021-03-29 21:49:21 +0300

    Generic Repository Design Pattern'a Geçiş

---
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> GetAll();
+        Product Get(int Id);
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+        List<Product> GetAllbyCategorysProduct(int categoryId);
+    }
+}
